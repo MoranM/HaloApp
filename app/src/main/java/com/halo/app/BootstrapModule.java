aa45213ef9b9bf9a66e0ed02cb224c16df6c3e91
MoranM <mishanmo@gmail.com>
2014-09-12 08:55:17 +0300

    starting point (from androidbootstrap)

---
+++ app/src/main/java/com/halo/app/BootstrapModule.java
@@ -0,0 +1,123 @@
+package com.halo.app;
+
+import android.accounts.AccountManager;
+import android.content.Context;
+
+import com.halo.app.authenticator.ApiKeyProvider;
+import com.halo.app.authenticator.BootstrapAuthenticatorActivity;
+import com.halo.app.authenticator.LogoutService;
+import com.halo.app.core.BootstrapService;
+import com.halo.app.core.Constants;
+import com.halo.app.core.PostFromAnyThreadBus;
+import com.halo.app.core.RestAdapterRequestInterceptor;
+import com.halo.app.core.RestErrorHandler;
+import com.halo.app.core.TimerService;
+import com.halo.app.core.UserAgentProvider;
+import com.halo.app.ui.BootstrapTimerActivity;
+import com.halo.app.ui.CheckInsListFragment;
+import com.halo.app.ui.MainActivity;
+import com.halo.app.ui.NavigationDrawerFragment;
+import com.halo.app.ui.NewsActivity;
+import com.halo.app.ui.NewsListFragment;
+import com.halo.app.ui.UserActivity;
+import com.halo.app.ui.UserListFragment;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.squareup.otto.Bus;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+
+/**
+ * Dagger module for setting up provides statements.
+ * Register all of your entry points below.
+ */
+@Module(
+        complete = false,
+
+        injects = {
+                BootstrapApplication.class,
+                BootstrapAuthenticatorActivity.class,
+                MainActivity.class,
+                BootstrapTimerActivity.class,
+                CheckInsListFragment.class,
+                NavigationDrawerFragment.class,
+                NewsActivity.class,
+                NewsListFragment.class,
+                UserActivity.class,
+                UserListFragment.class,
+                TimerService.class
+        }
+)
+public class BootstrapModule {
+
+    @Singleton
+    @Provides
+    Bus provideOttoBus() {
+        return new PostFromAnyThreadBus();
+    }
+
+    @Provides
+    @Singleton
+    LogoutService provideLogoutService(final Context context, final AccountManager accountManager) {
+        return new LogoutService(context, accountManager);
+    }
+
+    @Provides
+    BootstrapService provideBootstrapService(RestAdapter restAdapter) {
+        return new BootstrapService(restAdapter);
+    }
+
+    @Provides
+    BootstrapServiceProvider provideBootstrapServiceProvider(RestAdapter restAdapter, ApiKeyProvider apiKeyProvider) {
+        return new BootstrapServiceProvider(restAdapter, apiKeyProvider);
+    }
+
+    @Provides
+    ApiKeyProvider provideApiKeyProvider(AccountManager accountManager) {
+        return new ApiKeyProvider(accountManager);
+    }
+
+    @Provides
+    Gson provideGson() {
+        /**
+         * GSON instance to use for all request  with date format set up for proper parsing.
+         * <p/>
+         * You can also configure GSON with different naming policies for your API.
+         * Maybe your API is Rails API and all json values are lower case with an underscore,
+         * like this "first_name" instead of "firstName".
+         * You can configure GSON as such below.
+         * <p/>
+         *
+         * public static final Gson GSON = new GsonBuilder().setDateFormat("yyyy-MM-dd")
+         *         .setFieldNamingPolicy(LOWER_CASE_WITH_UNDERSCORES).create();
+         */
+        return new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+    }
+
+    @Provides
+    RestErrorHandler provideRestErrorHandler(Bus bus) {
+        return new RestErrorHandler(bus);
+    }
+
+    @Provides
+    RestAdapterRequestInterceptor provideRestAdapterRequestInterceptor(UserAgentProvider userAgentProvider) {
+        return new RestAdapterRequestInterceptor(userAgentProvider);
+    }
+
+    @Provides
+    RestAdapter provideRestAdapter(RestErrorHandler restErrorHandler, RestAdapterRequestInterceptor restRequestInterceptor, Gson gson) {
+        return new RestAdapter.Builder()
+                .setEndpoint(Constants.Http.URL_BASE)
+                .setErrorHandler(restErrorHandler)
+                .setRequestInterceptor(restRequestInterceptor)
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setConverter(new GsonConverter(gson))
+                .build();
+    }
+
+}
---
+++ integration-tests/src/main/java/com/halo/app/test/CarouselTest.java
@@ -0,0 +1,28 @@
+
+
+package com.halo.app.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.halo.app.ui.CarouselActivity;
+
+
+/**
+ * Test displaying of carousel.
+ */
+public class CarouselTest extends ActivityInstrumentationTestCase2<CarouselActivity> {
+
+    /**
+     * Create test for {@link CarouselActivity}
+     */
+    public CarouselTest() {
+        super(CarouselActivity.class);
+    }
+
+    /**
+     * Verify activity exists
+     */
+    public void testActivityExists() {
+        assertNotNull(getActivity());
+    }
+}
